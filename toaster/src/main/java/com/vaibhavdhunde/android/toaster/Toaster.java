2a6fffb06860e4e953caba93d55095a3ec71f7fe
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2018-11-06 13:33:45 +0530

    Remove initToaster method

--- toaster/src/main/java/com/vaibhavdhunde/android/toaster/Toaster.java
+++ toaster/src/main/java/com/vaibhavdhunde/android/toaster/Toaster.java
@@ -4,13 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public final class Toaster {
-    private Context mContext;
-
-    public void initToaster(Context context) {
-        mContext = context;
-    }
-
-    public void showToast(String message) {
-        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
